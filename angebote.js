c70d3b4a81fcc20bdadc27f2934a271381564eaf
Chris-XP <christiano3389@gmail.com>
2026-05-06 19:45:41 +0200

    Update

---
+++ angebote.js
@@ -0,0 +1,37 @@
+const angebote = [
+  {
+    bild: "images/haus1.jpg",
+    titel: "Schöne Wohnung",
+    ort: "Nordhorn",
+    beschreibung: "Helle Wohnung mit moderner Ausstattung und guter Lage.",
+    preis: "750 € / Monat"
+  },
+  {
+    bild: "images/haus2.jpg",
+    titel: "Modernes Haus",
+    ort: "Lingen",
+    beschreibung: "Großes Haus mit Garten, Garage und viel Platz.",
+    preis: "1.200 € / Monat"
+  },
+  {
+    bild: "images/haus3.jpg",
+    titel: "Zentrale Wohnung",
+    ort: "Meppen",
+    beschreibung: "Wohnung in zentraler Lage, ideal für Singles oder Paare.",
+    preis: "650 € / Monat"
+  },
+  {
+    bild: "images/haus4.jpg",
+    titel: "Familienhaus",
+    ort: "Bad Bentheim",
+    beschreibung: "Ruhige Lage, mehrere Zimmer und schöner Außenbereich.",
+    preis: "1.100 € / Monat"
+  },
+  {
+    bild: "images/haus5.jpg",
+    titel: "Kleine Wohnung",
+    ort: "Schüttorf",
+    beschreibung: "Kompakte Wohnung, perfekt für eine Person.",
+    preis: "500 € / Monat"
+  }
+];
