3afa38f970d6f699a31fb2f12261e6875677f70d
Chris-XP <christiano3389@gmail.com>
2026-05-06 20:33:18 +0200

    Update Aesthetic

--- angebote.js
+++ angebote.js
@@ -4,34 +4,39 @@ const angebote = [
     titel: "Schöne Wohnung",
     ort: "Nordhorn",
     beschreibung: "Helle Wohnung mit moderner Ausstattung und guter Lage.",
-    preis: "750 € / Monat"
+    preis: "750 € / Monat",
+    link: "https://www.kleinanzeigen.de/"
   },
   {
     bild: "images/haus2.png",
     titel: "Modernes Haus",
     ort: "Lingen",
     beschreibung: "Großes Haus mit Garten, Garage und viel Platz.",
-    preis: "1.200 € / Monat"
+    preis: "1.200 € / Monat",
+    link: "https://www.kleinanzeigen.de/"
   },
   {
     bild: "images/haus3.png",
     titel: "Zentrale Wohnung",
     ort: "Meppen",
     beschreibung: "Wohnung in zentraler Lage, ideal für Singles oder Paare.",
-    preis: "650 € / Monat"
+    preis: "650 € / Monat",
+    link: "https://www.kleinanzeigen.de/"
   },
   {
     bild: "images/haus4.png",
     titel: "Familienhaus",
     ort: "Bad Bentheim",
     beschreibung: "Ruhige Lage, mehrere Zimmer und schöner Außenbereich.",
-    preis: "1.100 € / Monat"
+    preis: "1.100 € / Monat",
+    link: "https://www.kleinanzeigen.de/"
   },
   {
     bild: "images/haus5.png",
     titel: "Kleine Wohnung",
     ort: "Schüttorf",
     beschreibung: "Kompakte Wohnung, perfekt für eine Person.",
-    preis: "500 € / Monat"
+    preis: "500 € / Monat",
+    link: "https://www.kleinanzeigen.de/"
   }
 ];
